85a0d289a14c982a002d86dabeebe10f6eb0d221
Oskar Tarka <oskartarka@MacBook-Air-Oskar.local>
2024-10-08 00:29:50 +0200

    I have reached the point of the lesson

--- src/main/java/pl/edu/pjatk/Projekt_MPR/controllers/MyRestController.java
+++ src/main/java/pl/edu/pjatk/Projekt_MPR/controllers/MyRestController.java
@@ -1,9 +1,31 @@
 package pl.edu.pjatk.Projekt_MPR.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.pjatk.Projekt_MPR.model.Computer;
+import pl.edu.pjatk.Projekt_MPR.services.ComputerService;
+
+import java.util.List;
 
 @RestController
 public class MyRestController {
+    private ComputerService computerService;
 
+    @Autowired
+    public MyRestController(ComputerService computerService) {
+        this.computerService = computerService;
+    }
 
+    @GetMapping("computer/all")
+    public List<Computer> getAll() {
+        return computerService.getAll();
+    }
+
+    @PostMapping("computer")
+    public void create(@RequestBody Computer computer){
+        this.computerService.createComputer(computer);
+    }
 }
