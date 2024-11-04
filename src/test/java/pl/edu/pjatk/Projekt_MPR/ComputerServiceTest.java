e006ca0fbe7e4ab6756c66ce532257fbccc2f74a
Oskar Tarka <oskar0208@gmail.com>
2024-11-04 14:52:00 +0100

    Testy lab 5

--- src/test/java/pl/edu/pjatk/Projekt_MPR/ComputerServiceTest.java
+++ src/test/java/pl/edu/pjatk/Projekt_MPR/ComputerServiceTest.java
@@ -8,9 +8,12 @@ import pl.edu.pjatk.Projekt_MPR.repository.ComputerRepository;
 import pl.edu.pjatk.Projekt_MPR.service.ComputerService;
 import pl.edu.pjatk.Projekt_MPR.service.StringUtilsService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class ComputerServiceTest {
     private ComputerService service;
@@ -32,5 +35,42 @@ public class ComputerServiceTest {
         verify(stringUtilsService,times(2)).upper(any());
         verify(computerRepository,times(4)).save(any());
     }
+
+    @Test
+    public void getAllComputersToLowerCaseWithFirstLetterAsCapital(){
+        Computer computer = new Computer("SIN","COS");
+        Computer computer2 = new Computer("MMM","OOO");
+
+        when(computerRepository.findAll()).thenReturn(Arrays.asList(computer,computer2));
+
+        service.getAll();
+
+        verify(stringUtilsService,times(4)).lowerExceptFirst(any());
+    }
+
+    @Test
+    public void getComputerByNameToLowercaseWithFirstLetterAsCapital(){
+        Computer computer = new Computer("SIN","COS");
+        Computer computer2 = new Computer("SIN","TG");
+        Computer computer3 = new Computer("MMM","OOO");
+
+        when(computerRepository.findByName(stringUtilsService.upper("sin"))).thenReturn(Arrays.asList(computer,computer2));
+        service.getComputerByName("sin");
+
+        verify(stringUtilsService,times(4)).lowerExceptFirst(any());
+    }
+
+    @Test
+    public void getComputerByComputerCaseModelToLowercaseWithFirstLetterAsCapital(){
+        Computer computer = new Computer("SIN","COS");
+        Computer computer2 = new Computer("MMM","TG");
+        Computer computer3 = new Computer("SIN","TG");
+
+        when(computerRepository.findByComputerCaseModel(stringUtilsService.upper("tg"))).thenReturn(Arrays.asList(computer2, computer3));
+        service.getComputerByComputerCaseModel(computer2.getComputerCaseModel());
+
+        verify(stringUtilsService,times(4)).lowerExceptFirst(any());
+    }
+
 
 }
